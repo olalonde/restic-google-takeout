37c3851ec3db51bc1b147d9a7bcefb95b2276ff6
Olivier Lalonde <o@syskall.com>
2026-03-09 00:55:16 +0800

    install restic from github

--- remote-scripts/install.sh
+++ remote-scripts/install.sh
@@ -2,8 +2,11 @@
 set -euo pipefail
 
 apt-get update -q
-apt-get install -y -q unzip restic curl
-restic self-update
+apt-get install -y -q unzip curl bzip2
+
+RESTIC_VERSION=$(curl -s https://api.github.com/repos/restic/restic/releases/latest | grep -oP '"tag_name": "v\K[^"]+')
+curl -fsSL "https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/restic_${RESTIC_VERSION}_linux_amd64.bz2" | bunzip2 > /usr/local/bin/restic
+chmod +x /usr/local/bin/restic
 curl -fsSL https://rclone.org/install.sh | bash
 
 DOCTL_VERSION=$(curl -s https://api.github.com/repos/digitalocean/doctl/releases/latest | grep -oP '"tag_name": "v\K[^"]+')
