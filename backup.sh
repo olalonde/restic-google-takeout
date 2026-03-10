86f9c9c36811f1b18408d11ed65fb1f13d00b593
Olivier Lalonde <o@syskall.com>
2026-03-10 08:36:13 +0800

    attempt to speed up unzipping on digitalocean

--- backup.sh
+++ backup.sh
@@ -16,10 +16,23 @@ rclone --config ./rclone.conf copy --progress gdrive:/Takeout ./Takeout
 step_end=$(date +%s)
 log "rclone download done ($(( step_end - step_start ))s)"
 
-# We unzip all zip files so that we can better leverage restic's deduplication
+# We unzip all zip files so that we can better leverage restic's deduplication.
+
+# Optimization for DigitalOcean: each zip is extracted to a temporary directory
+# on the local SSD (/tmp) first, then moved to the destination on the slower
+# mounted volume. This avoids read/write thrashing on the mounted volume during
+# extraction.
 log "Starting unzip"
 step_start=$(date +%s)
-find ./Takeout -name "*.zip" -exec sh -c 'unzip -o "$1" -d "$(dirname "$1")" && rm "$1"' _ {} \;
+find ./Takeout -name "*.zip" -exec sh -c '
+  zip="$1"
+  dest="$(dirname "$zip")"
+  tmp=$(mktemp -d /tmp/takeout-unzip.XXXXXX)
+  unzip -o "$zip" -d "$tmp" && \
+  mv "$tmp"/* "$dest/" && \
+  rm -rf "$tmp" && \
+  rm "$zip"
+' _ {} \;
 step_end=$(date +%s)
 log "Unzip done ($(( step_end - step_start ))s)"
 
